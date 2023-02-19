07d863e2cbcc330ad45c6171bdc56ba71f6b3823
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-18 23:37:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a YouTube downloader bot. Send me a YouTube video link and I'll send you a download link.")
 
-def download_video(url):
+def download_video(url, file_type, downloads_path):
     yt = YouTube(url)
     stream = yt.streams.get_highest_resolution()
-    return stream
+    if file_type == "mp3":
+        stream = stream.streams.filter(only_audio=True).first()
+    return stream.download(downloads_path)
 
 def send_video(update, context):
     try:
         video_url = update.message.text
-        video = download_video(video_url)
-        with io.BytesIO() as video_buffer:
-            video.stream_to_buffer(video_buffer)
-            video_buffer.seek(0)
-            context.bot.send_video(chat_id=update.message.chat_id, video=video_buffer)
+        file_type = "mp4"  # or "mp3"
+        downloads_path = os.path.join(os.getcwd(), "temp")
+        video_path = download_video(video_url, file_type, downloads_path)
+        with open(video_path, 'rb') as f:
+            if file_type == "mp4":
+                context.bot.send_video(chat_id=update.message.chat_id, video=f)
+            elif file_type == "mp3":
+                context.bot.send_audio(chat_id=update.message.chat_id, audio=f)
     except Exception as e:
         context.bot.send_message(chat_id=update.message.chat_id, text=f"Error: {e}")
 
