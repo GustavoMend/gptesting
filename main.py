4200999c52b2a453bfef71f96ee2a9d7007abef6
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-19 00:03:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
 import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+import tempfile
+import telebot
 from pytube import YouTube
 
-# This function handles the /start command
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a YouTube downloader bot. Send me a YouTube video link and I'll send you a download link.")
+# Initialize the Telegram bot
+bot = telebot.TeleBot("YOUR_TELEGRAM_BOT_TOKEN")
 
-# This function handles all other messages
-def echo(update, context):
-    url = update.message.text
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Welcome to the video downloader bot! Send me the YouTube video URL and the desired format (mp4 or mp3) separated by a space.")
 
-    # Check if the message contains a valid YouTube URL
-    if "youtube.com" in url:
-        try:
-            yt = YouTube(url)
-            # Assign the file type and downloads path
-            file_type = "mp4"
-            downloads_path = os.path.join(os.getcwd(), "temp")
-            # Try downloading the converted video
-            video = yt.streams.get_highest_resolution().download(output_path=downloads_path, filename="video")
-            file_path = os.path.join(downloads_path, f"{video.title}.{video.mime_type.split('/')[-1]}")
-            context.bot.send_document(chat_id=update.effective_chat.id, document=open(file_path, 'rb'))
-        except Exception as e:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Video could not be downloaded.")
-            print(e)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Send me a YouTube video link.")
-
-# Create the bot and add the handlers
-updater = Updater(token="5542310588:AAHg4m7EzQzB7j5cSllnf7qZUpkwqpwyWl4", use_context=True)
-dispatcher = updater.dispatcher
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-echo_handler = MessageHandler(filters.Filters.text & (~filters.Filters.command), echo)
-
-dispatcher.add_handler(echo_handler)
+@bot.message_handler(func=lambda message: True)
+def download_video(message):
+    # Parse the user's message for the video URL and desired format
+    try:
+        url, file_type = message.text.split()
+    except ValueError:
+        bot.reply_to(message, "Please send the video URL and the desired format (mp4 or mp3) separated by a space.")
+        return
+    
+    # Try converting a url to a downloadable video
+    try:
+        yt = YouTube(url)
+    except Exception:
+        bot.reply_to(message, "Video URL is not valid.")
+        return
+    
+    # Assign the file type and donwloads path
+    downloads_path = tempfile.gettempdir()
+    file_type = file_type.lower()
+    if file_type not in ["mp4", "mp3"]:
+        bot.reply_to(message, "Invalid file format. Please choose mp4 or mp3.")
+        return
+    
+    # Try downloading the converted video
+    try:
+        video = yt.streams.filter(file_extension=file_type).order_by('resolution').desc().first()
+        file_path = os.path.join(downloads_path, video.default_filename) 
+        video.download(downloads_path)
+    except Exception:
+        bot.reply_to(message, "Video could not be downloaded.")
+        return
+    
+    # Send the video file to the user
+    with open(file_path, "rb") as file:
+        bot.send_video(message.chat.id, file)
 
 # Start the bot
-updater.start_polling()
+bot.polling()
