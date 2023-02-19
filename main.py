959020681e3626b65ac581e936e23ac289dc71cf
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-19 09:42:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
 import os
-import tempfile
-import telebot
-import subprocess
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from pytube import YouTube
 
-# Initialize the Telegram bot
-bot = telebot.TeleBot("5542310588:AAHg4m7EzQzB7j5cSllnf7qZUpkwqpwyWl4")
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Assign the downloads path
-downloads_path = tempfile.mkdtemp()
+# Define command handlers
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Send me a YouTube video link and I'll download the audio for you.")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Welcome to the YouTube audio downloader bot! Send me the YouTube video URL.")
-
-@bot.message_handler(func=lambda message: True)
-def download_audio(message):
-    # Parse the user's message for the video URL
+def download_audio(update, context):
+    url = update.message.text
     try:
-        url = message.text
-    except ValueError:
-        bot.reply_to(message, "Please send the video URL.")
-        return
-
-    # Use youtube-dl to download the audio from the video
-    try:
-        output_file = os.path.join(downloads_path, "%(title)s.%(ext)s")
-        command = ["youtube-dl", "-x", "--audio-format", "mp3", "-o", output_file, url]
-        subprocess.check_output(command, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        bot.reply_to(message, f"Error downloading audio: {e.output.decode('utf-8')}")
-        return
-
-    # Send the audio file to the user
-    try:
-        audio_file_path = output_file.replace("%(title)s.%(ext)s", "*.mp3")
-        with open(audio_file_path, "rb") as file:
-            bot.send_audio(message.chat.id, file)
+        # Get the YouTube video object
+        yt = YouTube(url)
+        # Get the audio stream
+        stream = yt.streams.filter(only_audio=True).first()
+        # Download the audio
+        filename = f"{yt.title}.mp3"
+        stream.download(filename=filename)
+        # Send the audio file to the user
+        context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(filename, 'rb'))
+        # Delete the downloaded file
+        os.remove(filename)
     except Exception as e:
-        bot.reply_to(message, f"Error sending file: {str(e)}")
-    finally:
-        # Clean up the downloaded files
-        for file in os.listdir(downloads_path):
-            if file.endswith(".mp3"):
-                os.remove(os.path.join(downloads_path, file))
+        logger.error(f"Error downloading audio: {e}")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, there was an error downloading the audio. Please try again.")
+
+def error(update, context):
+    logger.warning(f"Update {update} caused error {context.error}")
+
+# Set up the Telegram bot
+token = 'YOUR_BOT_TOKEN_HERE'
+updater = Updater(token, use_context=True)
+
+# Set up command handlers
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'^https?:\/\/(www.youtube.com|youtu.be)\/'), download_audio))
+
+# Set up error handler
+updater.dispatcher.add_error_handler(error)
 
 # Start the bot
-bot.polling()
+updater.start_polling()
+updater.idle()
