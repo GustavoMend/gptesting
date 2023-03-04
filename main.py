5bc7558d0ad3a32aba90015cf214fb9d24fbeec9
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-03-04 18:30:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,55 +10,40 @@ app = Client("my_bot", bot_token="6054512042:AAE-WQ5IdL18KhVZkSLO1e_zECKG3XKztck
 def start_handler(client, message):
     message.reply_text("Hello! Send me a YouTube video URL and I'll convert it to MP3 for you.")
 
-@app.on_message(filters.text)
-def youtube_handler(client, message):
+# filter function to check if the message contains a valid YouTube URL
+@bot.on_message(filters.private & filters.regex(r"(http|https)://(www\.)?youtube\.com/watch\?v=\w+"))
+async def download_and_send_audio(client, message):
+    # extract the YouTube URL from the message
     url = message.text.strip()
-    if not url.startswith('https://www.youtube.com/watch?v='):
-        message.reply_text("Invalid URL. Please send a valid YouTube video URL.")
-        return
 
+    # download and convert the video to audio
     try:
-        download_and_convert(url)
+        yt = pytube.YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        output_mp4 = f"{yt.title}.mp4"
+        audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
+        audio_clip = AudioFileClip(output_mp4)
+        output_mp3 = f"{yt.title}.mp3"
+        audio_clip.write_audiofile(output_mp3)
+        audio_clip.close()
+        os.remove(output_mp4)
+        title = yt.title
+        artist = yt.author
+        album = ""
+        update_metadata(output_mp3, title, artist, album)
     except Exception as e:
-        message.reply_text(f"Error: {str(e)}")
+        await message.reply_text(f"An error occurred while downloading the video: {str(e)}")
         return
 
-    # Send the MP3 file
-    title = pytube.YouTube(url).title
-    mp3_file = f"{title}.mp3"
-    message.reply_audio(audio=mp3_file)
+    # send the audio file to the user
+    try:
+        await message.reply_audio(audio=output_mp3, title=title, performer=artist, caption=url)
+        os.remove(output_mp3)
+    except Exception as e:
+        await message.reply_text(f"An error occurred while sending the audio file: {str(e)}")
 
-def download_and_convert(url):
-    # Create YouTube object and extract audio stream
-    yt = pytube.YouTube(url)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-
-    # Set output file names
-    output_mp4 = f"{yt.title}.mp4"
-
-    # Download audio stream to MP4 file
-    audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
-
-    # Use moviepy to convert the MP4 file to an MP3 file with metadata support, then delete the MP4 file
-    video_clip = VideoFileClip(output_mp4)
-    audio_clip = video_clip.audio
-    output_mp3 = f"{yt.title}.mp3"
-    audio_clip.write_audiofile(output_mp3, method='chunk')
-    video_clip.close()
-    os.remove(output_mp4)
-
-    # Get video details from YouTube
-    title = yt.title
-    artist = yt.author
-    album = ""
-
-    # Update metadata of MP3 file
-    update_metadata(output_mp3, title, artist, album)
-
-    print(f"Download complete. Audio saved as {output_mp3}.")
-
+# function to update the metadata of an audio file
 def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
-    # Update the file metadata according to YouTube video details
     with open(file_path, 'r+b') as file:
         media_file = mutagen.File(file, easy=True)
         media_file["title"] = title
@@ -67,4 +52,5 @@ def update_metadata(file_path: str, title: str, artist: str, album: str="") -> N
         media_file["artist"] = artist
         media_file.save(file)
 
-app.run()
+# start the bot
+bot.run()
