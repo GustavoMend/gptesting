ebbc90adc03d68138bef101917c93ce6d4633928
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-03-04 22:22:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-from moviepy.editor import AudioFileClip
 import os
 import mutagen
-import youtube_dl
+from moviepy.editor import AudioFileClip
+import yt_dlp
 
 def download_and_convert(url):
-    # Set ytdl options
-    ytdl_opts = {
+    # Extract audio stream information from YouTube
+    ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': '%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
-        }]
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+        'quiet': True,
+        'no_warnings': True,
+        'forcejson': True,
     }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+    audio_file_path = f"{info_dict['title']}.mp3"
 
-    # Create YouTube object and extract audio stream
-    try:
-        with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=True)
-            output_mp3 = ydl.prepare_filename(info_dict)
-    except youtube_dl.utils.DownloadError:
-        print("The provided URL is not supported.")
-        return
-
-    # Get video details from YouTube
-    title = info_dict.get('title', '')
-    artist = info_dict.get('uploader', '')
-    album = ''
+    # Download audio stream to MP3 file using yt_dlp
+    ydl_opts['outtmpl'] = audio_file_path
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
     # Update metadata of MP3 file
-    update_metadata(output_mp3, title, artist, album)
+    title = info_dict['title']
+    artist = info_dict.get('artist', '')
+    album = info_dict.get('album', '')
+    update_metadata(audio_file_path, title, artist, album)
 
-    print(f"Download complete. Audio saved as {output_mp3}.")
+    print(f"Download complete. Audio saved as {audio_file_path}.")
 
-def update_metadata(file_path: str, title: str, artist: str, album: str='') -> None:
+def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
     # Update the file metadata according to YouTube video details
     with open(file_path, 'r+b') as file:
         media_file = mutagen.File(file, easy=True)
-        media_file['title'] = title
-        if album:
-            media_file['album'] = album
-        media_file['artist'] = artist
-        media_file.save()
+        media_file["TIT2"] = title  # Title
+        media_file["TPE1"] = artist  # Artist
+        media_file["TALB"] = album  # Album
+        media_file.save(file)
 
 url = input("Enter YouTube video URL: ")
 download_and_convert(url)
